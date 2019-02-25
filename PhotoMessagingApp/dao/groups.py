ab7db6a22b994d4f9a6c9f7e73c12157d7db4ae2
Bernardo2207 <33329311+Bernardo2207@users.noreply.github.com>
2019-02-25 15:03:09 -0400

    Update groups.py

--- PhotoMessagingApp/dao/groups.py
+++ PhotoMessagingApp/dao/groups.py
@@ -1,34 +1,48 @@
-
 class GroupsDAO:
 
     def getAllGroups(self):
-        result = []
+        result = [[30,"Avengers","Steve Rogers"],[11,"Los Extraterrestres","Wisin y Yandel"],[9,"All-Stars","Fulanito"]]
         return result
 
     def getGroupById(self, gid):
-        result = []
+        result=[]
+        if gid==30:
+            result=[30,"Avengers","Steve Rogers"]
+
+        elif gid==11:
+            result=[11,"Los Extraterrestres","Wisin y Yandel"]
+        elif gid== 9:
+            result=[9,"All-Stars","Fulanito"]
+
         return result
 
     def getGroupByGroupName(self, gname):
-        result = []
+        result=[]
+        if gname=="Avengers":
+            result = [[30, "Avengers", "Steve Rogers"]]
+        elif gname=="Los Extraterrestres":
+            result=[[11,"Los Extraterrestres","Wisin y Yandel"]]
+        elif gname=="All-Stars":
+            result=[[9,"All-Stars","Fulanito"]]
+
         return result
 
     def getAllGroupsByOwnerID(self, ownerID):
-        result = []
+        result = [[10,"Basket"],[12,"Playa Sabado"]]
         return result
 
     def getGroupMembers(self, gid):
-        result = []
+        result = [["Pepe","Aguilar"],["Carmen", "Sandiego"]]
         return result
 
     def createGroup(self):
-        result = []
+        result = "Group Succesfully Created."
         return result
 
     def deleteGroup(self, gid):
-        result = []
+        result = "the group %d has been deleted" %(gid)
         return result
 
     def updateGroup(self, gid):
-        result = []
+        result = "the group %d has been updated" %(gid)
         return result
