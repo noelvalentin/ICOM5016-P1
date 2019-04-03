33fffc89d8fbc70755e133e602441e38e0798401
Bernardo2207 <33329311+Bernardo2207@users.noreply.github.com>
2019-04-02 20:59:50 -0400

    DAO changes
    Added missing methods

--- PhotoMessagingApp/dao/users.py
+++ PhotoMessagingApp/dao/users.py
@@ -1,6 +1,10 @@
 
 class UsersDAO:
 
+    def login(self):
+        result= "Welcome"
+        return result
+
     def getAllUsers(self):
         result = [[0, "Fulano", "Apellido", 787, "hotmail", "password123"],
                   [1, "Manuel", "Rodriguez", 939, "gmail", "qwerty"]
@@ -8,23 +12,45 @@ class UsersDAO:
         return result
 
     def getUserById(self, uid):
-        result = []
+        vacio=[]
+        if uid== 0:
+            result = [0, "Fulano", "Apellido", 787, "hotmail", "password123"]
+        elif uid ==1:
+            result=[1, "Manuel", "Rodriguez", 939, "gmail", "qwerty"]
+        else:
+                return vacio
         return result
 
     def getUserByFirstName(self, firstName):
-        result = []
+        result=[]
+        if firstName=="Manuel":
+            result=[1, "Manuel", "Rodriguez", 939, "gmail", "qwerty"]
+        elif firstName=="Fulano":
+            result = [0, "Fulano", "Apellido", 787, "hotmail", "password123"]
         return result
 
     def getUserByLastName(self, lastName):
         result = []
+        if lastName == "Rodriguez":
+            result = [1, "Manuel", "Rodriguez", 939, "gmail", "qwerty"]
+        elif lastName == "Apellido":
+            result = [0, "Fulano", "Apellido", 787, "hotmail", "password123"]
         return result
 
     def getUserByPhone(self, phone):
         result = []
+        if phone == 939:
+            result = [1, "Manuel", "Rodriguez", 939, "gmail", "qwerty"]
+        elif phone == 787:
+            result = [0, "Fulano", "Apellido", 787, "hotmail", "password123"]
         return result
 
     def getUserByEmail(self, email):
         result = []
+        if email == "gmail":
+            result = [1, "Manuel", "Rodriguez", 939, "gmail", "qwerty"]
+        elif email == "hotmail":
+            result = [0, "Fulano", "Apellido", 787, "hotmail", "password123"]
         return result
 
     def getUserGroupsById(self, uid):
@@ -32,5 +58,17 @@ class UsersDAO:
         return result
 
     def getUserContactsById(self, uid):
-        result = []
+        result = [["Jose","Verga",878,"yut@hotmail.com"],["Pepe","Last",878,"lagata@hotmail.com"]]
+        return result
+
+    def deleteUser(self, uid):
+
+        result="USER DESTROYED"
+        return result
+
+    def updateUser(self, uid):
+        result= "User %d information has been updated" %(uid)
+        return result
+    def createUser(self):
+        result="User created,please check yur email to verify"
         return result
